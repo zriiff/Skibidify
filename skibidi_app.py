30fd9024e0803a159e27900cf3180da7010e0752
R-uuu <122701893+R-uuu@users.noreply.github.com>
2024-10-05 19:36:51 -0400

    added all the python files for skibidify

---
+++ skibidi_app.py
@@ -0,0 +1,2 @@
+# this will be the file that takes the video and produces the skibidi version for the website
+import skibid_script
---
+++ skibidi_script.py
@@ -0,0 +1,1 @@
+#this will be the file that contains all the importatnt functions
