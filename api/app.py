ead94fefe929524093949bc3bfea1e8423888df4
zriiff <zriiff@gmail.com>
2024-10-05 20:36:18 -0400

    added api

---
+++ api/app.py
@@ -0,0 +1,14 @@
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route("/api/skibidify", methods=["GET"])
+def skibidify():
+    console.log
+    name = request.args.get("name", "World")
+    return jsonify({'message': f"Hello {name}"})
+
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
