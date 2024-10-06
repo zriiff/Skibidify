ddaf324938edd53c70d3adccc901dd945178e7fb
R-uuu <122701893+R-uuu@users.noreply.github.com>
2024-10-05 20:10:12 -0400

    first changes

--- skibidi_script.py
+++ skibidi_script.py
@@ -1,1 +1,4 @@
-#this will be the file that contains all the importatnt functions
+#this will be the file that contains all the importantt functions
+import moviepy
+
+user_video = moviepy.VideoFileClip("").subclip(0, 10)
